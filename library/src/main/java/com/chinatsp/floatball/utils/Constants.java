89028e0a43e8d8dc044a57fd9583c8f1acc08211
litao <litao@chinatsp.com>
2020-03-09 14:13:53 +0800

    添加注释

--- library/src/main/java/com/chinatsp/floatball/utils/Constants.java
+++ library/src/main/java/com/chinatsp/floatball/utils/Constants.java
@@ -1,12 +1,24 @@
 package com.chinatsp.floatball.utils;
 
 public class Constants {
+    /**
+     * 悬浮球展开后距离上下的边距
+     */
     public static final int FLOAT_LAYOUT_MARGIN_H = 100;
+    /**
+     * 悬浮球展开后距离左右的边距
+     */
     public static final int FLOAT_LAYOUT_MARGIN_W = 100;
     /**
      * 悬浮球阴影宽度
      */
     public static final int FLOAT_SHADOW = 13;
+    /**
+     * 悬浮球的宽高
+     */
     public static final int FLOAT_BALL_W_H = 116;
+    /**
+     * 悬浮球展开后的宽高
+     */
     public static final int FLOAT_LAYOUT_W_H = 300;
 }
